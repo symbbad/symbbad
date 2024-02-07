37bcc90e571e81972b5247a6e9b5aa1663013951
Hyunjun Sim <symbbad@gmail.com>
2024-02-07 12:52:26 +0900

    Update settings.py
    Debug on/off => off

--- main/main/settings.py
+++ main/main/settings.py
@@ -36,7 +36,7 @@ def get_secret(setting, secret=secret):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
